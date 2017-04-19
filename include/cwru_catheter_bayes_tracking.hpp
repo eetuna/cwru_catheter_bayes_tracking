01c7d918145292c4d3d76d3f6ebfdcaa79c33dbc
eetuna <eet12@case.edu>
2017-04-19 15:39:26 -0400

    bayes parent class

--- include/cwru_catheter_bayes_tracking.hpp
+++ include/cwru_catheter_bayes_tracking.hpp
@@ -12,15 +12,15 @@ namespace Bayes_filter
     // Allow use of a short name for matrix namespace
     namespace FM = Bayes_filter_matrix;
 
-/*
- * Abstraction support classes, at the base of the hierarchy
- */
+    /*
+    * Abstraction support classes, at the base of the hierarchy
+    */
 
-class Bayes_base {
-/*
- * A very abstract Polymorphic base representation!
- * Interface provides: type, internal, error handing, and destruction
- */
+    class Bayes_base {
+    /*
+    * A very abstract Polymorphic base representation!
+    * Interface provides: type, internal, error handing, and destruction
+    */
     public:
         typedef Bayes_filter_matrix::Float Float;
         // Type used throughout as a number representation for state etc
@@ -28,7 +28,40 @@ class Bayes_base {
         virtual ~Bayes_base() = 0;
         // Polymorphic
 
-};
+    };
+
+
+    /*
+     * Abstract Prediction models
+     *  Predict models are used to parametrize predict functions of filters
+     */
+    class Predict_model_base : public Bayes_base
+    {
+        // Empty
+    };
+
+    class Sampled_predict_model : virtual public Predict_model_base
+    /* Sampled stochastic predict model
+     * x*(k) = fw(x(k-1), w(k))
+     * fw should generate samples from the stochastic variable w(k).
+     * This fundamental model is used instead of the predict likeligood function L(x*|x)
+     * Since drawinf samples from an arbitrary L is non-trivial (see MCMC theory)
+     * the burden is place on the model to generate these samples.
+     * Defines an interface without data members
+     */
+
+    {
+    public:
+        virtual const FM::Vec& fw(const FM::Vec& x) const = 0;
+        // Note: Reference return value as a speed optimization, MUST be copied by caller.
+
+    };
+
+    class Functional
+
+
+
+
 
 
 }
--- src/cwru_catheter_bayes_tracking.cpp
+++ src/cwru_catheter_bayes_tracking.cpp
@@ -2,4 +2,13 @@
 // Created by eet12 on 4/19/17.
 //
 
-#include "cwru_catheter_bayes_tracking/cwru_catheteR_bayes_tracking.hpp"
+#include "cwru_catheter_bayes_tracking/cwru_catheter_bayes_tracking.hpp"
+
+/* Filter namespace */
+namespace Bayesian_filter
+{
+    Bayes_base::~Bayes_base()
+    /* Default definition required for a pure virtual destructor
+     * */
+    {}
+}
